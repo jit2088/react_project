ce31a4e0436b3617b3f122a1299032eb926d3469
jin <jin.2088@usask.ca>
2019-07-06 10:53:10 -0600

    radio, checkbox works

--- travel/src/App.js
+++ travel/src/App.js
@@ -11,16 +11,24 @@ class App extends Component{
             lastName:"",
             age:"",
             gender:"",
-            destination:""
+            destination:"",
+            isVegan: false,
+            isKosher:false,
+            isLocatoseFree: false
             
         }
         this.handleChange = this.handleChange.bind(this)
     }
 
     handleChange(event){
-        const {name, value} = event.target
-        this.setState({
-          [name]: value
+        const {name, value, type, checked} = event.target
+        type === "checkbox" ?
+          this.setState({
+            [name]:checked
+          })
+          :
+          this.setState({
+            [name]: value
         })
     }
     render(){
@@ -46,30 +54,84 @@ class App extends Component{
                         placeholder = "Age: " /><br/>
                     
                     <input 
-                        name="gender" 
-                        value = {this.state.gender} 
-                        onChange={this.handleChange} 
-                        placeholder = "Gender: " /><br/>
-                    
-                    <input 
                         name="destination" 
                         value = {this.state.destination} 
                         onChange={this.handleChange} 
-                        placeholder = "Destination: " /><br/>
+                        placeholder = "Destination: " /><br/><br/>
 
-                    {}
+                    <label>
+                      <input 
+                        type="radio"
+                        name ="gender"
+                        value="male"
+                        checked ={this.state.gender === "male"}
+                        onChange = {this.handleChange}/>Male
+                      <br/>
+                      <input 
+                        type="radio"
+                        name ="gender"
+                        value="female"
+                        checked ={this.state.gender === "female"}
+                        onChange = {this.handleChange}/> Female
+                      
+                    </label>
+
                     <br/>
-                    {}
-                    <button>Submit</button>
+                    <select 
+                        value = {this.state.destination} 
+                        name="destination" 
+                        onChange = {this.handleChange}>
+                        <option value = "">--- Please Choose a destination ---</option>
+                        <option value = "sermany">S</option>
+                        <option value = "hermany">H</option>
+                        <option value = "eermany">E</option>
+                        
+                    </select>
+
+                  <br/>
+                  <label>
+                    <input  
+                      type = "checkbox"
+                      name = "isVegan"
+                      onChange = {this.handleChange}
+                      checked = {this.state.isVegan} 
+                      /> Vegan
+                    <br/>
+                    <input  
+                      type = "checkbox"
+                      name = "isKosher"
+                      onChange = {this.handleChange}
+                      checked = {this.state.iisKosher}
+                      /> Kosher
+                    <br/>
+                    <input  
+                      type = "checkbox"
+                      name = "isLocatoseFree"
+                      onChange = {this.handleChange}
+                      checked = {this.state.isLocatoseFree}
+                      /> Locatose Free
+                  </label>
+                  <br/>
+                  <br/>
+                  <button>Submit</button>
 
                 </form>
-                <hr/>
+                
                 <br/>
+                <hr/>
                 <h2>Entered information: </h2>
                 <p>Your name: {this.state.firstName}{this.state.lastName}</p>
                 <p>Your age: {this.state.age}</p>
                 <p>Your gender: {this.state.gender}</p>
                 <p>Your destination: {this.state.destination}</p>
+                <p>Your dietary restrictions : 
+                  <br/>
+                  Kosher: {this.state.isKosher ? "Yes" : "No"}
+                  <br/>
+                  LocatoseFree: {this.state.isLocatoseFree ? "Yes" : "No"}
+                  <br/>
+                  Vegan: {this.state.isVegan ? "Yes" : "No"}</p>
+
 
             </main>
         )
