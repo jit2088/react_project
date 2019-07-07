89cc48bd1ff3e4c760d5c4895b95074664cfe8b0
jin <jin.2088@usask.ca>
2019-07-06 23:45:33 -0600

    get header styles works

---
+++ meme/src/MemeGenerator.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react"
+
+
+class MemeGenerator extends Component{
+    constructor(){
+        super()
+        this.state={}
+    }
+    render(){
+
+    return(
+        <h1>ME ME GENERATOR SECTION</h1>
+    )
+    }
+}
+
+export default MemeGenerator
